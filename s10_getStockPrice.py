15b3e02bad1fa83ad904c4367d8823c2702e80a3
full0308 <144349220+full0308@users.noreply.github.com>
2023-09-07 21:57:03 +0900

    Add files via upload

---
+++ s10_getStockPrice.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+# 종목코드 딕셔너리
+stocks = {'에스티큐브':{'code':'052020'}, 'SK하이닉스':{'code':'000660'}, '삼성생명':{'code':'032830'},'에코프로':{'code':'086520'}}
+
+def setPrice(stocks):
+    for _key, _value in stocks.items():
+        url = 'https://finance.naver.com/item/main.nhn?code=' + _value['code']
+
+        response = requests.get(url)
+        response.raise_for_status()
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+
+        today = soup.select_one('#chart_area > div.rate_info > div')
+        price = today.select_one('.blind')
+        stocks[_key]['price']= price.get_text()
+    return stocks
+
+
+if __name__ == '__main__':
+    stocks=setPrice(stocks)
+    print(stocks)
